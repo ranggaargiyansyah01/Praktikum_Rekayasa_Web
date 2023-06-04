3bd3dc22c2d1c9933a009c84bc0e3ce9a6bc82f4
ranggaargiyansyah <132808081+ranggaargiyansyah01@users.noreply.github.com>
2023-06-04 21:19:47 +0700

    OkeFix

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
+
+class MahasiswaController extends Controller
+{
+    public function read()
+{
+    $model=new Mahasiswa();
+
+    if(!$datax=$model ->all()) {
+        $success=false;
+        $message = "Database Eror";
+    }
+    
+    foreach($datax as $dt) {
+        $data[]=[
+            'nim'=>$dt->nim,
+            'nama'=>$dt->nama,
+            'umur'=>$dt->umur,
+            'alamat'=>$dt->alamat,
+            'kota'=>$dt->kota,
+            'kelas'=>$dt->kelas,
+            'jurusan'=>$dt->jurusan,
+
+        ];
+    }
+    if (!empty($data)) {
+        $success = true;
+        $message = "Data berhasil dibaca";
+    } else
+        {
+            $success = false;
+            $message = "Data tidak ditemukan/kosong";
+        }
+    $balikan = [
+        "success"=> $success,
+        "message"=> $message,
+        "data"=> @$data
+
+    ];
+
+    return response() -> json($balikan);
+    }
+
+
+    public function create(Request $req)
+    {
+        $model=new Mahasiswa();
+        $model->nim=$req->nim;
+        $model->nama=$req->nama;
+        $model->umur=$req->umur;
+        $model->alamat=$req->alamat;
+        $model->kota=$req->kota;
+        $model->kelas=$req->kelas;
+        $model->jurusan=$req->jurusan;
+        if($model->save()) {
+            $success=true;
+            $message="Data berhasil disimpan";
+        }
+
+        $balikan = [
+            "success"=>$success,
+            "message"=>$message,
+            "data" => @$req->all()
+        ];
+
+        return response() ->json($balikan);
+    }
+    
+
+}
+
